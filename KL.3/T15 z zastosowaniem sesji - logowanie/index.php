ade094b3435b3e270b76bdbf03cbe044acc4b75a
Ivan <iwanowsky1@wp.pl>
2024-01-29 11:23:38 +0100

    commit

---
+++ KL.3/T15 z zastosowaniem sesji - logowanie/index.php
@@ -0,0 +1,34 @@
+
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>T15</title>
+</head>
+<body>
+<h2>Autor: Łukasz Iwaniec 3ip_2</h2>
+<hr>
+<p>
+    PHP - zastosowanie sesji - logowanie
+<br>
+    Na podstawie przykładu w podręczniku wykonaj projekt strony wykorzystującej mechanizm logowania. Przejście do strony
+    z właściwą treścią powinno nastąpić dopiero po prawidłowym podaniu nazwy użytkownika i hasła.
+</p>
+<hr>
+<br>
+<form action="index.php" method="post">
+    Podaj nazwisko i imię:<br>
+    <input type="text" name="nazw" value=" " size="35">
+    <p><input type="submit" value="Wyślij" name="wyslij"></p>
+</form>
+<?php
+
+
+
+
+?>
+</body>
+</html>
